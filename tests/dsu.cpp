5e30e93833f70575daa68a5327815636f5f94920
Ivan Smirnov <ifsmirnov@yandex.ru>
2017-02-26 01:50:22 +0300

    dsu test

---
+++ tests/dsu.cpp
@@ -0,0 +1,23 @@
+#define BOOST_TEST_MAIN
+#include <boost/test/included/unit_test.hpp>
+#include "../jngen.h"
+
+using jngen::Dsu;
+
+BOOST_AUTO_TEST_CASE(dsu) {
+    Dsu d;
+
+    BOOST_CHECK(d.connected());
+
+    d.getParent(4);
+    BOOST_CHECK(!d.connected());
+
+    BOOST_CHECK(d.link(1, 2));
+    BOOST_CHECK(d.link(3, 4));
+    BOOST_CHECK(d.link(1, 4));
+    BOOST_CHECK(!d.link(2, 3));
+    BOOST_CHECK(d.link(0, 3));
+    BOOST_CHECK(d.connected());
+
+    BOOST_CHECK(!d.link(0, 0));
+}
