01d369403dae92e6f0f0aa08c2839493a4d7dcb3
Ivan Smirnov <ifsmirnov@yandex.ru>
2017-02-25 04:03:10 +0300

    Make Random methods non-static

--- random.h
+++ random.h
@@ -7,14 +7,22 @@
 
 namespace impl {
 
-std::mt19937 randomEngine;
-
 static void assertRandomEngineConsistency() {
     std::mt19937 engine(1234);
     ensure(engine() == 822569775);
     ensure(engine() == 2137449171);
     ensure(engine() == 2671936806);
 }
+
+class Random;
+
+class BaseTypedRandom {
+public:
+    BaseTypedRandom(Random& random) : random(random) {}
+
+protected:
+    Random& random;
+};
 
 template<typename T>
 struct TypedRandom;
@@ -23,40 +31,41 @@ class Random {
 public:
     Random() {
         assertRandomEngineConsistency();
-        randomEngine.seed(std::random_device{}());
-
-        static bool created = false;
-        ensure(!created, "impl::Random should be created only once");
-        created = true;
+        seed(std::random_device{}());
     }
 
-    void seed(size_t val) {
-        randomEngine.seed(val);
+    void seed(uint32_t val) {
+        randomEngine_.seed(val);
+    }
+
+    void seed(const std::vector<uint32_t>& seed) {
+        std::seed_seq seq(seed.begin(), seed.end());
+        randomEngine_.seed(seq);
     }
 
     uint32_t next() {
-        return randomEngine();
+        return randomEngine_();
     }
 
     int next(int n) {
         // TODO(ifsmirnov): make random more uniform
-        return randomEngine() % n;
+        return randomEngine_() % n;
     }
 
     long long next(long long n) {
         // TODO(ifsmirnov): make random more uniform
-        return ((static_cast<long long>(randomEngine()) << 32)
-            ^ randomEngine()) % n;
+        return ((static_cast<long long>(randomEngine_()) << 32)
+            ^ randomEngine_()) % n;
     }
 
     size_t next(size_t n) {
         // TODO(ifsmirnov): make random more uniform
-        return ((static_cast<unsigned long long>(randomEngine()) << 32)
-            ^ randomEngine()) % n;
+        return ((static_cast<unsigned long long>(randomEngine_()) << 32)
+            ^ randomEngine_()) % n;
     }
 
     double next(double n) {
-        return (double)randomEngine() / randomEngine.max() * n;
+        return (double)randomEngine_() / randomEngine_.max() * n;
     }
 
     int next(int l, int r) {
@@ -80,68 +89,77 @@ public:
     }
 
     template<typename T, typename ... Args>
-    static T tnext(Args... args) {
-        return TypedRandom<T>::next(args...);
+    T tnext(Args... args) {
+        return TypedRandom<T>{*this}.next(args...);
     }
 
     template<typename ... Args>
-    static std::pair<int, int> nextp(Args... args) {
+    std::pair<int, int> nextp(Args... args) {
         return tnext<std::pair<int, int>>(args...);
     }
+
+private:
+    std::mt19937 randomEngine_;
 };
 
 Random rnd;
 
 template<>
-struct TypedRandom<int> {
-    static int next(int n) { return rnd.next(n); }
-    static int next(int l, int r) { return rnd.next(l, r); }
+struct TypedRandom<int> : public BaseTypedRandom {
+    using BaseTypedRandom::BaseTypedRandom;
+    int next(int n) { return random.next(n); }
+    int next(int l, int r) { return random.next(l, r); }
 };
 
 template<>
-struct TypedRandom<double> {
-    static double next(double n) { return rnd.next(n); }
-    static double next(double l, double r) { return rnd.next(l, r); }
+struct TypedRandom<double> : public BaseTypedRandom {
+    using BaseTypedRandom::BaseTypedRandom;
+    double next(double n) { return random.next(n); }
+    double next(double l, double r) { return random.next(l, r); }
 };
 
 template<>
-struct TypedRandom<long long> {
-    static long long next(long long n) { return rnd.next(n); }
-    static long long next(long long l, long long r) { return rnd.next(l, r); }
+struct TypedRandom<long long> : public BaseTypedRandom {
+    using BaseTypedRandom::BaseTypedRandom;
+    long long next(long long n) { return random.next(n); }
+    long long next(long long l, long long r) { return random.next(l, r); }
 };
 
 template<>
-struct TypedRandom<size_t> {
-    static size_t next(size_t n) { return rnd.next(n); }
-    static size_t next(size_t l, size_t r) { return rnd.next(l, r); }
+struct TypedRandom<size_t> : public BaseTypedRandom {
+    using BaseTypedRandom::BaseTypedRandom;
+    size_t next(size_t n) { return random.next(n); }
+    size_t next(size_t l, size_t r) { return random.next(l, r); }
 };
 
 struct OrderedPairTag {} opair;
 
 template<>
-struct TypedRandom<std::pair<int, int>> {
-    static std::pair<int, int> next(int n) {
-        // can't write 'return {rnd.next(n), rnd.next(n)}' because order of
+struct TypedRandom<std::pair<int, int>> : public BaseTypedRandom {
+    using BaseTypedRandom::BaseTypedRandom;
+
+    std::pair<int, int> next(int n) {
+        // can't write 'return {random.next(n), random.next(n)}' because order of
         // evaluation of function arguments is unspecified.
-        int first = rnd.next(n);
-        int second = rnd.next(n);
+        int first = random.next(n);
+        int second = random.next(n);
         return {first, second};
     }
-    static std::pair<int, int> next(int l, int r) {
-        int first = rnd.next(l, r);
-        int second = rnd.next(l, r);
+    std::pair<int, int> next(int l, int r) {
+        int first = random.next(l, r);
+        int second = random.next(l, r);
         return {first, second};
     }
 
-    static std::pair<int, int> next(int n, OrderedPairTag) {
+    std::pair<int, int> next(int n, OrderedPairTag) {
         return ordered(next(n));
     }
-    static std::pair<int, int> next(int l, int r, OrderedPairTag) {
+    std::pair<int, int> next(int l, int r, OrderedPairTag) {
         return ordered(next(l, r));
     }
 
 private:
-    static std::pair<int, int> ordered(std::pair<int, int> pair) {
+    std::pair<int, int> ordered(std::pair<int, int> pair) const {
         if (pair.first > pair.second) {
             std::swap(pair.first, pair.second);
         }
@@ -151,15 +169,20 @@ private:
 
 } // namespace impl
 
+using impl::Random;
+
 using impl::rnd;
 using impl::opair;
 
 void registerGen(int argc, char *argv[]) {
-    std::uint64_t val = 0;
+    std::vector<uint32_t> seed;
     for (int i = 1; i < argc; ++i) {
+        int startPosition = seed.size();
+        seed.emplace_back();
         for (char *s = argv[i]; *s; ++s) {
-            val = val * 10099 + *s;
+            ++seed[startPosition];
+            seed.push_back(*s);
         }
     }
-    rnd.seed(val);
+    rnd.seed(seed);
 }
