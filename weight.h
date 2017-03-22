f1a88f7b9f1fc0674764b57f0779d12465fc6c39
Ivan Smirnov <ifsmirnov@yandex.ru>
2017-03-23 02:40:56 +0300

    forgotten file

---
+++ weight.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <utility>
+
+#include "variant.h"
+#include "variant_array.h"
+
+namespace jngen {
+
+#define JNGEN_DEFAULT_WEIGHT_TYPES int, double, std::string, std::pair<int, int>
+
+#if defined(JNGEN_EXTRA_WEIGHT_TYPES)
+#define JNGEN_WEIGHT_TYPES JNGEN_DEFAULT_WEIGHT_TYPES , JNGEN_EXTRA_WEIGHT_TYPES
+#else
+#define JNGEN_WEIGHT_TYPES JNGEN_DEFAULT_WEIGHT_TYPES
+#endif
+
+using Weight = Variant<JNGEN_WEIGHT_TYPES>;
+using WeightArray = VariantArray<JNGEN_WEIGHT_TYPES>;
+
+} // namespace jngen
+
+using jngen::Weight;
+using jngen::WeightArray;
